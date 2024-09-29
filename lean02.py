d363bb6e540427ab4f3cba056d2eee98ffd68595
Aphidech <aphidech12780@gmail.com>
2024-09-29 22:48:59 +0700

    No.62-63

--- lean02.py
+++ lean02.py
@@ -5,10 +5,33 @@
 # print(tuple1)
 
 #12.61
-sumx=0
-tuple1 = tuple(x for x in range(1,11))
-n_tuple1 = len(tuple1)
-for i in range(n_tuple1):
-    if(i%2==0):
-        sumx = sumx + tuple1[i]**2
-print(sumx)
+# sumx=0
+# tuple1 = tuple(x for x in range(1,11))
+# n_tuple1 = len(tuple1)
+# for i in range(n_tuple1):
+#     if(i%2==0):
+#         sumx = sumx + tuple1[i]**2
+# print(sumx)
+
+#12.62
+# sumx=0
+# tuple1 = tuple(x for x in range(1,11))
+# n_tuple1 = len(tuple1)
+# for i in range(n_tuple1):
+#     if(i%2==1):
+#         sumx = sumx + tuple1[i]**2
+# print(sumx)
+
+#12.63 Prime Number in tuple
+tuple1 = (x for x in range(1,26))
+# print(tuple1)
+for num in tuple1:
+    count=0
+    for i in range(1,num+1):
+        # print(num,i)
+        if (num%i==0):
+            count = count +1
+            
+    if(count==2):
+        print(num)
+        
