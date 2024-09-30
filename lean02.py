dcf40c4577d57f62496f7b87d6d5b6bdac2df8e9
Aphidech <aphidech12780@gmail.com>
2024-09-30 22:02:36 +0700

    python No.66

--- lean02.py
+++ lean02.py
@@ -1,9 +1,19 @@
 
 
-#12.65 Input fine tuple
-tuple1 = (x for x in range(1,10))
+# #12.65 Input fine tuple
+# tuple1 = (x for x in range(1,10))
+# input_num = int(input("number :"))
+# if input_num in tuple1:
+#     print("Yes")
+# else:
+#     print("No")
+
+#12.66 len tuple
+tuple1 = tuple([x for x in range(1,10)])
 input_num = int(input("number :"))
-if input_num in tuple1:
-    print("Yes")
-else:
-    print("No")
+if input_num > len(tuple1):
+    print("input more than len")
+elif input_num < len(tuple1): 
+    print("input less than len")
+else :
+    print("Is equal")
